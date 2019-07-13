69a95f9b40adf9747909019c26364a4c015dfd4e
Ramon Almeida <ramondeallima@gmail.com>
2019-07-13 18:43:05 -0300

    Classes de modelo da camada BLL criadas.

---
+++ BibliotecaGames.BLL/Modelo/Genero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGames.BLL.Modelo
+{
+    public class Genero : ID
+    {
+        public string Descricao { get; set; }
+    }
+}
---
+++ BibliotecaGames.BLL/Modelo/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaGames.BLL.Modelo
+{
+    public class Jogo : ID
+    {
+        public double ValorPago { get; set; }
+        public string Imagem { get; set; }
+        public DateTime DataCompra { get; set; }
+    }
+}
